82684096ac08baf6f688f651ec9ae45a55f85c2e
Marquitos <marquitosbbb19@gmail.com>
2025-11-15 18:01:13 -0600

    Commit one

---
+++ RANDOM (GENRADOR DE DATOS ALEATORIOS)/Camiones.py
@@ -0,0 +1,26 @@
+import random 
+
+
+Camión 3 ½ toneladas 2
+
+Camión 5 toneladas 2
+
+Camión Rabón 3
+
+Camión Torton 1
+
+Plataforma 2
+
+Caja seca 53’ 2
+
+Reefer 1
+
+Full doble 1
+
+Lowboy 1
+
+Góndola 1
+
+Pipa 1
+
+
---
+++ RANDOM (GENRADOR DE DATOS ALEATORIOS)/Productos.py
@@ -0,0 +1,15 @@
+Alimentos y bebidas
+Farmacéutica y salud
+Textil y moda
+Tecnología y electrónica
+Automotriz
+Construcción
+Química y petroquímica
+Minería y metalurgia
+Agricultura y ganadería
+E-commerce y paquetería
+Muebles y línea blanca
+Industria del entretenimiento
+Hotelería y turismo
+Papel, cartón y empaques
+Logística de residuos
