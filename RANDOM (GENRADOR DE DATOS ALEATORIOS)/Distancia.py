97c1f5acb333c7c20e8e420f5423738a043530b4
Marquitos <marquitosbbb19@gmail.com>
2025-11-20 18:59:11 -0600

    Cvs ciudades y Random echos

--- RANDOM (GENRADOR DE DATOS ALEATORIOS)/Distancia.py
+++ RANDOM (GENRADOR DE DATOS ALEATORIOS)/Distancia.py
@@ -1,1 +1,15 @@
-import random 
+import pandas as pd
+import random
+
+df = pd.read_csv("Ciudades.csv")
+
+fila = df.sample(1).iloc[0]
+
+print("=== Selección aleatoria ===")
+print(f"ID: {fila['id']}")
+print(f"Ciudad: {fila['ciudad']}")
+print(f"Estado: {fila['estado']}")
+print(f"Latitud: {fila['latitud']}")
+print(f"Longitud: {fila['longitud']}")
+print(f"Distancia KM: {fila['distancia_km']}")
+print(f"Tiempo Horas: {fila['tiempo_horas']}")
